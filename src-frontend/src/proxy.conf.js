cc8e4f984e0afc11ee51d0f260104523e5cdc669
diolkaee <eikeniklasmeyer@hotmail.de>
2021-05-13 16:04:46 +0200

    Bugfix: The PORT environment variable is now parsed correctly into the server-URL

--- src-frontend/src/proxy.conf.js
+++ src-frontend/src/proxy.conf.js
@@ -2,7 +2,7 @@ const PROXY_CONFIF = [{
     context: [
         "/api"
     ],
-    "target": process.env.PORT || "http://localhost:8080",
+    "target": "http://localhost:" + process.env.PORT || 8080,
     "secure": false    
 }]
 
